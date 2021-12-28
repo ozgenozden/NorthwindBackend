02e0f662d8bc0773203223a62657a144b7dfff7f
Ozgen Ozden <ozgen.ozden@dijitalsahne.com>
2021-12-28 17:12:47 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,56 @@
+﻿using Business.Abstract;
+using Business.Contants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal productDal;
+        public ProductManager(IProductDal _productDal)
+        {
+            productDal = _productDal;
+        }
+        public IResult Add(Product product)
+        {
+            productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
+        }
+
+        public IResult Delete(Product product)
+        {
+            productDal.Delete(product);
+            return new SuccessResult(Messages.ProductDeleted);
+        }
+
+        public IDataResult<Product> GetById(int productId)
+        {
+          
+            return new SuccessDataResult<Product>(productDal.Get(p => p.ProductId==productId));
+        }
+
+        public IDataResult<List<Product>> GetList()
+        {
+            return new SuccessDataResult<List<Product>>(productDal.GetList());
+        }
+
+        public IDataResult<List<Product>> GetListByCategory(int categoryId)
+        {
+            return new SuccessDataResult<List<Product>>(productDal.GetList(p=>p.CategoryId==categoryId));
+        }
+
+        public IResult Update(Product product)
+        {
+           productDal.Update(product);
+           return new SuccessResult(Messages.ProductUpdated);
+        }
+
+        
+    }
+}
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,83 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IProductService productService;
+        public ProductsController(IProductService _productService)
+        {
+            productService = _productService;
+        }
+        [HttpGet("getall")]
+        public IActionResult GetList()
+        {
+            var result=productService.GetList();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpGet("getlistbycategoryid")]
+        public IActionResult GetByCategoryId(int catagoryId)
+        {
+            var result = productService.GetListByCategory(catagoryId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int productId)
+        {
+            var result = productService.GetById(productId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result=productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(Product product)
+        {
+            var result = productService.Update(product);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(Product product)
+        {
+            var result = productService.Delete(product);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
